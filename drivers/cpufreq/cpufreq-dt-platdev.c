1d26a419dc50f310aae8fa9510194709b868d48b
Romuald JEANNE <romuald.jeanne@st.com>
2020-01-30 15:28:05 +0100

    ARM stm32mp1 r3 CPUFREQ

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -121,6 +121,7 @@ static const struct of_device_id blacklist[] __initconst = {
 
 	{ .compatible = "st,stih407", },
 	{ .compatible = "st,stih410", },
+	{ .compatible = "st,stm32mp157", },
 
 	{ .compatible = "sigma,tango4", },
 
