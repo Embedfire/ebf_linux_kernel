0f2153e02a61dd10e45eac9d48215b8dea0b1bce
laoyening <1337849843@qq.com>
2020-02-18 01:34:49 +0000

    wifi驱动进一步完善，经测试可用

--- make_deb.sh
+++ make_deb.sh
@@ -6,8 +6,7 @@ build_opts="${build_opts} ARCH=arm"
 build_opts="${build_opts} KBUILD_DEBARCH=${DEBARCH}"
 build_opts="${build_opts} LOCALVERSION=-imx-r1"
 build_opts="${build_opts} KDEB_CHANGELOG_DIST=${deb_distro}"
-build_opts="${build_opts} KDEB_PKGVERSION=1${DISTRO}"
-	
+build_opts="${build_opts} KDEB_PKGVERSION=1${DISTRO}"	
 build_opts="${build_opts} KDEB_SOURCENAME=linux-upstream"
 make ${build_opts}  npi_v7_defconfig
 make ${build_opts}  menuconfig
