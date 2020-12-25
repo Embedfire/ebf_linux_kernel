4d9b21ec0cd5d314b13c9c988b2de3768af433f9
laoyening <1337849843@qq.com>
2020-12-25 14:41:30 +0800

    driver:support ebf imx6ull touchscreen
    Signed-off-by: laoyening <1337849843@qq.com>

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -949,6 +949,7 @@ static const struct of_device_id goodix_of_match[] = {
 	{ .compatible = "goodix,gt9271" },
 	{ .compatible = "goodix,gt928" },
 	{ .compatible = "goodix,gt967" },
+	{ .compatible = "goodix,gt9xx" },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, goodix_of_match);
