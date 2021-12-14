21e972d3773a31cfe634c3430384f9b9fb17c9c7
nebulamagic <892461474@qq.com>
2021-12-14 02:46:20 +0000

    clear build

--- build_image/build/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1stable */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1stable SMP PREEMPT Mon Dec 13 07:46:27 UTC 2021"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "dev131.embedfire.local"
-#define LINUX_COMPILER "gcc version 7.5.0 (Ubuntu/Linaro 7.5.0-3ubuntu1~18.04)"
