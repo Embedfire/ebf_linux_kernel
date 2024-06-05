987d97fe8de1c42356fe194d9935ee3989c66b90
hyw66686 <1920906857@qq.com>
2024-06-05 08:19:29 +0000

    修改构建脚本，规范内核名字，减少默认编译线程

--- make_deb.sh
+++ make_deb.sh
@@ -1,12 +1,12 @@
 deb_distro=bionic
 DISTRO=stable
-build_opts="-j 46"
+build_opts="-j 16"
 build_opts="${build_opts} O=build_image/build"
 build_opts="${build_opts} ARCH=arm"
 build_opts="${build_opts} KBUILD_DEBARCH=${DEBARCH}"
 build_opts="${build_opts} LOCALVERSION=-stm-r1"
 build_opts="${build_opts} KDEB_CHANGELOG_DIST=${deb_distro}"
-build_opts="${build_opts} KDEB_PKGVERSION=1${DISTRO}"
+build_opts="${build_opts} KDEB_PKGVERSION=1.$(date +%g%m)${DISTRO}"
 build_opts="${build_opts} CROSS_COMPILE=arm-linux-gnueabihf-" 
 build_opts="${build_opts} KDEB_SOURCENAME=linux-upstream"
 make ${build_opts}  stm32mp157_ebf_defconfig
