38ddf9a41b493c9c935927043c26ca5b14c41971
hyw66686 <1920906857@qq.com>
2024-06-05 07:31:15 +0000

    修改构建脚本，规范内核名字、内核版本

--- make_deb.sh
+++ make_deb.sh
@@ -1,12 +1,12 @@
 deb_distro=bionic
 DISTRO=stable
-build_opts="-j 6"
+build_opts="-j 16"
 build_opts="${build_opts} O=build_image/build"
 build_opts="${build_opts} ARCH=arm"
 build_opts="${build_opts} KBUILD_DEBARCH=${DEBARCH}"
-build_opts="${build_opts} LOCALVERSION=-carp-imx6"
+build_opts="${build_opts} LOCALVERSION=-imx6"
 build_opts="${build_opts} KDEB_CHANGELOG_DIST=${deb_distro}"
-build_opts="${build_opts} KDEB_PKGVERSION=1${DISTRO}"
+build_opts="${build_opts} KDEB_PKGVERSION=1.$(date +%g%m)${DISTRO}"
 build_opts="${build_opts} CROSS_COMPILE=arm-linux-gnueabihf-" 	
 build_opts="${build_opts} KDEB_SOURCENAME=linux-upstream"
 make ${build_opts}  npi_v7_defconfig
